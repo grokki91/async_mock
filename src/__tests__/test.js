3dbd6885a0a16eac2e1b391d3658b721b71d314c
Pavel Chekalin <grokki91@gmail.com>
2023-01-27 01:25:49 +0300

    Change test

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,5 +1,5 @@
 import GameSavingLoader from '../app/GameSavingLoader';
-import read from '../app/reader';
+import * as read from '../app/reader';
 
 test(('Check class GameSavingLoader'), (done) => {
   const expectedResult = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
@@ -10,11 +10,7 @@ test(('Check class GameSavingLoader'), (done) => {
 });
 
 test(('Check class GameSavingLoader with mock'), async () => {
-  jest.mock('../app/reader.js');
-  try {
-    read.mockRejectedValueOnce(new Error('Error'));
-    await GameSavingLoader.load();
-  } catch (error) {
-    expect(error).toThrow();
-  }
+  const readMock = jest.spyOn(read, 'default');
+  readMock.mockRejectedValueOnce(new Error('Error'));
+  await expect(GameSavingLoader.load()).rejects.toThrow();
 });
